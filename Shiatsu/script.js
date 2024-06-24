2b6c4a95b0d241f295b6d3163c408fa08a2e30db
chrisxffz <166542843+chrisxffz@users.noreply.github.com>
2024-06-24 16:43:35 -0300

    Delete Shiatsu directory

--- Shiatsu/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    var menuIcon = document.querySelector('.menu-icon');
-    var navbar = document.getElementById('navbar');
-    menuIcon.classList.toggle('change');
-    navbar.classList.toggle('active');
-}

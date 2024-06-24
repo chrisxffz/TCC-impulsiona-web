ab179a2b4e4aea90d10fc5c4967502ee9f4d2861
chrisxffz <166542843+chrisxffz@users.noreply.github.com>
2024-06-24 16:50:36 -0300

    Delete site directory

--- site/massagem.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    var menuIcon = document.querySelector('.menu-icon');
-    var navbar = document.getElementById('navbar');
-    menuIcon.classList.toggle('change');
-    navbar.classList.toggle('active');
-}
-

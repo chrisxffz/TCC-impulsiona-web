9b134be723c6a1ffaab9a6cbeed57e9437f4cb62
chrisxffz <166542843+chrisxffz@users.noreply.github.com>
2024-06-24 16:52:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,43 +5,18 @@ function toggleMenu() {
     navbar.classList.toggle('active');
 }
 
-let currentIndex = 0;
-const carrosel = document.querySelector('.carrosel');
-const images = document.querySelectorAll('.imagem');
-const totalImages = images.length;
 
-document.querySelector('.back').addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = totalImages - 1;
-    }
-    updateCarrosel();
-});
 
-document.querySelector('.forward').addEventListener('click', () => {
-    if (currentIndex < totalImages - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    updateCarrosel();
-});
 
-function updateCarrosel() {
-    const offset = -currentIndex * 100;
-    carrosel.style.transform = `translateX(${offset}%)`;
-}
-
-function startAutoSlide() {
-    setInterval(() => {
-        if (currentIndex < totalImages - 1) {
-            currentIndex++;
-        } else {
-            currentIndex = 0;
-        }
-        updateCarrosel();
-    }, 3000); // Muda de imagem a cada 3 segundos
-}
-
-startAutoSlide();
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    keyboard: true,
+  });
